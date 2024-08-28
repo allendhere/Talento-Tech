9414a1b614d6875fa2647916fc59b3abd8abb07f
allendhere <bolivarvjd@gmail.com>
2024-08-27 19:47:24 -0500

    cree el codigo correcto

--- proyecto/llamar dataset.py
+++ proyecto/llamar dataset.py
@@ -4,8 +4,8 @@ import csv
 archivo_csv = 'proyecto/heart_attack_dataset.csv'
 
 # Inicializamos contadores
-contador_male = 0
-contador_female = 0
+LifestyleChanges = 0
+Angioplasty = 0
 
 with open(archivo_csv, encoding='latin1') as fichero_csv:
     lector = csv.reader(fichero_csv)
@@ -13,13 +13,15 @@ with open(archivo_csv, encoding='latin1') as fichero_csv:
     next(lector)
 
     # Recorremos las filas del archivo
-    for fila in lector:
+    for column in lector:
         # La primera columna está en la posición 0
-        valor_genero = fila[0]
+        valor_genero = column[7]
 
-        if valor_genero.lower() == 'male':
-            contador_male += 1
-        elif valor_genero.lower() == 'female':
-            contador_female += 1
+        if valor_genero.lower() == 'lifestyle changes':
+            LifestyleChanges += 1
+        elif valor_genero.lower() == 'angioplasty':
+            Angioplasty += 1
 
-print(f"Hay {contador_male} registros 'Male' y {contador_female} registros 'Female'.")
+        
+
+print(f"Hay {LifestyleChanges} registros 'lifestyle changes' y {Angioplasty} registros 'angioplasty'.")
