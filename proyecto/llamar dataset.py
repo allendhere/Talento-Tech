8fa8137224fa7329bf416a1c350d227f0f7fb1a5
allendhere <bolivarvjd@gmail.com>
2024-09-02 19:43:26 -0500

    analicis de la ultima columna

--- proyecto/llamar dataset.py
+++ proyecto/llamar dataset.py
@@ -6,7 +6,7 @@ archivo_csv = 'proyecto/heart_attack_dataset.csv'
 # Inicializamos contadores
 Lifestyle_Changes = 0
 Angioplasty = 0
-
+Coronary_Artery_Bypass_Graft_CABG = 0
 Medication = 0
 
 with open(archivo_csv, encoding='latin1') as fichero_csv:
@@ -23,12 +23,12 @@ with open(archivo_csv, encoding='latin1') as fichero_csv:
             Lifestyle_Changes += 1
         elif valor_genero.lower() == 'angioplasty':
             Angioplasty += 1
-            
-
+        elif valor_genero.lower() == 'coronary artery bypass graft (cabg)':
+            Coronary_Artery_Bypass_Graft_CABG += 1
         elif valor_genero.lower() == 'medication':
             Medication += 1
 
 print(f"{Lifestyle_Changes} registros 'Lifestyle Changes'.")
 print(f"{Angioplasty} registros 'Angioplasty'.")
-
+print(f"{Coronary_Artery_Bypass_Graft_CABG} registros 'Coronary Artery Bypass Graft (CABG)'")
 print(f"{Medication} registros 'Medication'.")
