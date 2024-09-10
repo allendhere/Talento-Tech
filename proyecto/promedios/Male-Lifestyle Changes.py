7ce70f0eed6c296922ab6db7749da8e2f0bd4981
allendhere <bolivarvjd@gmail.com>
2024-09-10 17:48:47 -0500

    1mer-tratamiento

---
+++ proyecto/promedios/Male-Lifestyle Changes.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('C:/Users/boliv/OneDrive/Escritorio/github/Talento-Tech/proyecto/heart_attack_dataset.csv',encoding='latin1')
+
+
+# Filtramos las filas correspondientes al tratamiento "Lifestyle Changes" y género masculino
+tratamiento_lifestyle_hombres = df[(df['Treatment'] == 'Lifestyle Changes') & (df['Gender'] == 'Male')]
+
+# Contamos cuántos pacientes masculinos han recibido este tratamiento
+cantidad_hombres_lifestyle = len(tratamiento_lifestyle_hombres)
+
+print(f"La cantidad de hombres que han recibido el tratamiento 'Lifestyle Changes' es: {cantidad_hombres_lifestyle}")
